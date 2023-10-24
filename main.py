4a9a7a1b6bce96134b35db0576e3f6b9fee49549
Terézia Sliacka <terezia.sliacka@m2ms.sk>
2023-10-24 18:17:35 +0200

    04 - otacanie lodky

--- main.py
+++ main.py
@@ -7,8 +7,21 @@ HEIGHT = 500
 
 perfectShip = Ship(WIDTH/2, HEIGHT/2)
 
+def on_key_down(key):
+    #otacanie
+    if key == keys.RIGHT:
+        perfectShip.turning = -5
+
+def on_key_up(key):
+    #otacanie
+    if key == keys.RIGHT:
+        perfectShip.turning = 0
+
+def update():
+    perfectShip.update()
 
 def draw():
+    screen.clear()
     perfectShip.draw()
 
 pgzrun.go()
--- ship.py
+++ ship.py
@@ -5,4 +5,11 @@ class Ship(Actor):
     def __init__(self, x, y):
         super(Ship, self).__init__('ship')
         self.pos = (x,y)
+        self.turning = 0
+
+    def update(self):
+        #self.angle = self.angle + self.turning
+        self.angle += self.turning
+
+
 
