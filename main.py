0226d5fe343382ecfeacc20e2d916d668c2f07a2
Terézia Sliacka <terezia.sliacka@m2ms.sk>
2023-10-24 18:00:09 +0200

    02 - trieda Ship, main, vytvorenie instancie vesmirnej lode

---
+++ main.py
@@ -0,0 +1,10 @@
+import pgzrun
+
+from ship import Ship
+
+perfectShip = Ship()
+
+def draw():
+    perfectShip.draw()
+
+pgzrun.go()
---
+++ ship.py
@@ -0,0 +1,5 @@
+from pgzhelper import *
+
+class Ship(Actor):
+    def __init__(self):
+        super(Ship, self).__init__('ship')
